570d6e7f8543e473d5e615cf9c7fda1f7aa3fe11
erenefdc <134430650+erenefdc@users.noreply.github.com>
2025-09-09 19:07:01 +0300

    Add files via upload

---
+++ pushable/components/comments.tsx
@@ -0,0 +1,25 @@
+﻿"use client";
+
+import Giscus from "@giscus/react";
+
+export default function Comments() {
+    return (
+        <div className="mt-8">
+            <h2 className="text-xl font-bold mb-4">Leave a comment 💭</h2>
+            <Giscus
+                repo="erenefdc/autoinstall" 
+                repoId="R_kgDOPsJPlA"
+                category="General"
+                categoryId="DIC_kwDOPsJPlM4CvMsf"
+                mapping="pathname"
+                strict="0"
+                reactionsEnabled="1"
+                emitMetadata="1"
+                inputPosition="top"
+                theme="preferred_color_scheme"
+                lang="en"
+                loading="lazy"
+            />
+        </div>
+    );
+}
